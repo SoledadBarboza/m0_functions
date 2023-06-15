6c86ea70442ebbf4b1de5af7ad7e23849c923ecb
Dan <cute.dan@gmail.com>
2023-06-15 17:20:06 -0400

    M0 Functions

---
+++ functions.js
@@ -0,0 +1,32 @@
+// Defining Our Own Functions
+// For each exercise below, write the function according to the requirements. 
+// Call each method at least twice and store the return value in a variable. 
+// Use console.log() to see the return value in the console.
+
+// 1: Write a function named greeting that returns a string with a general greeting. 
+
+
+// 2: Write a function named customGreeting that returns a greeting WITH a specific name.
+
+
+// 3: Write a function named greetPerson that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
+
+
+// 4: Write a function named square that takes in one number, and returns the square of that number.
+// BONUS: Print a sentence that interpolates the return value of your square function.
+
+
+// 5: Write a function named checkStock that satisfies the following interaction pattern:
+// Hint: You will only write one checkStock function that checks the quantity and then prints the corresponding statement.
+
+checkStock(4, "Coffee");
+// => "Coffee is stocked"
+
+checkStock(3, "Tortillas");
+// => "Tortillas - running LOW"
+
+checkStock(0, "Cheese");
+// => "Cheese - OUT of stock!"
+
+checkStock(1, "Salsa");
+// => "Salsa - running LOW"
---
+++ selfEval.md
@@ -0,0 +1,4 @@
+# Self-Evaluation
+
+- Reflect on your learning and work today. What are you most proud of?
+- In your own words, why are functions a powerful tool in our applications?
